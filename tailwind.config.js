b28d88e705a38e556ec69a2e0e6118fa0415b78c
wwwrt <tpripon99@gmail.com>
2023-11-16 21:18:33 +0200

    stylized pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        g1: '#ccd5ae', 
+        g2: '#e9edc9',
+        y1: '#fefae0',
+        y2: '#faedcd',
+        m1: '#e7c8a0',
+        m2: '#d4a373'
+
+      },
+
+    },
   },
   plugins: [],
 }
